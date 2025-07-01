2f1e99ac51b09cf2cbd8e036be070c0976903607
Mounika-K4 <kaniyampatimounika2003@gmail.com>
2025-07-02 01:39:39 +0530

    data types.py

---
+++ classwork/3.Data Types.py
@@ -0,0 +1,73 @@
+#Integer Type
+a= (20)
+b=30
+print(a,b)
+print (type(a))
+print(type(b))
+
+#floating point
+a=5.2
+b=10.5
+print(a)
+print(b)
+print(type(a))
+print(type(b))
+
+#Complex value
+x=4+6j
+y=2+4j
+print(x,y)
+print(type(x))
+print(type(y))
+
+#string type
+m='hai'
+n="hello"
+o='''hlo'''
+print(m,n,o)
+print(type(m))
+print(type(n))
+print(type(o))
+
+l=list()
+i=[1,2,3,4,5,6]
+print(i)
+print(type(i))
+
+#tuple type
+t=tuple()
+p=(1,2,3,4)
+print(p)
+print(type(p))
+
+#Set type
+s=set()
+m={10,20,30,40,}
+print(m)
+print(type(m))
+
+
+#frozenset type
+z=frozenset({10,20,30,40})
+print(z)
+print(type(z))
+
+#dictionary type
+d=dict()
+g={'name':"mounika",'age':"21"}
+print(g)
+print(type(g))
+
+#boolen type
+u=True
+v=False
+print(u,v)
+print(type(u))
+print(type(v))
+
+#None type
+d=None
+print(d)
+print(type(d))
+
+
