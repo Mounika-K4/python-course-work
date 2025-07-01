d24681d2abaaeb782b9df9aa6bbc45ccf2afb6b2
Mounika-K4 <kaniyampatimounika2003@gmail.com>
2025-07-01 23:14:41 +0530

    variables and assignment

---
+++ classwork/2.Variable and Assignment.py
@@ -0,0 +1,50 @@
+myvar=10
+print(myvar)
+myVar=10
+print(myVar)
+my_var=10
+print(my_var)
+_myvar=10
+print(myvar)
+
+#case-sensitive
+Myvar=10
+myvar=100
+print(Myvar)
+print(myvar)
+
+#Basic Assignment
+a=10
+b=20
+print(a)
+print(b)
+
+#Multiple Assignment
+a,b,c=100,200,300
+print(a)
+print(b)
+print(c)
+
+#Same value Assignment
+a=b=c=100
+print(a)
+print(b)
+print(c)
+
+#Swapping variable
+a=50
+b=60
+a,b=b,a
+print(a)
+print(b)
+
+#Deleting Assignment
+a=10
+b=20
+del(a)
+print(b)
+
+a=30
+b=40
+del(b)
+print(a)
