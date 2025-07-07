1c49b9e340f8821390f8715895e47388c4e2b880
Mounika-K4 <kaniyampatimounika2003@gmail.com>
2025-07-07 21:33:29 +0530

    strings

--- classwork/5. Strings.py
+++ classwork/5. Strings.py
@@ -61,3 +61,152 @@ print(ord(X))      #output : 64
 print(chr(97))    #output : a
 print(chr(45))    #output : -
 print(chr(81))    #output : Q
+
+#3. String Case Conversion Methods
+#Uppercase()
+name="Mounika"
+print(name.upper())   #Output:MOUNIKA
+#Lowercase()
+name="MOUNI"
+print(name.lower())   #Output:mouni
+
+#capitalize()
+name="deekshitha"
+print(name.capitalize())   #Output:Deekshitha
+
+#title()
+name="string methods"
+print(name.title())     #Output:string methods
+
+#swapcase()
+name="aBcDeFgHiJ"
+print(name.swapcase())    #Output:AbCdEfGhIj
+
+#Casefold()
+name="B"
+print(name.casefold())   #Output:b
+
+#4.Alignment & Formating Method
+#center()
+name='mounika'
+print(name.center(15,'*'))    #Output: ***mounika****
+name="mounika"
+print(name.center(25,"$"))    #Output: $$$$$$$$$mounika$$$$$$$$$
+
+#ljust()
+name="mouli"
+print(name.ljust(10,"^"))     #Output: mouli^^^^^
+
+#rjust()
+name="sudha"
+print(name.rjust(10,"_"))    #Output: _____sudha
+
+#zfill()
+a="583"
+print(a.zfill(10))      #Output: 0000000583
+
+#5.Search & Find Methods
+#find()
+name="manohar"
+print(name.find("a"))          #Output: 1
+print(name.find("h"))          #Output: 4
+print(name.find("m"))          #Output: 0
+
+#rfind()
+name="abcdabcdabcdabcd"
+print(name.rfind("c"))        #Output: 14
+
+#index()
+name="python"
+print(name.index("h"))        #Output: 3
+print(name.index("n"))        #Output: 5
+
+#rindex()
+name="abcdabcdabcdabcd"
+print(name.rindex("b"))        #Output: 13
+print(name.rindex("c"))        #Output: 14
+
+#count()
+name="pythoncourse"
+print(name.count("o"))        #output: 2
+print(name.count("c"))        #output: 1
+
+#6.String Testing Methods
+#startswith()
+name="21G21A0208"
+print(name.startswith("21"))         #Output: True
+print(name.startswith("22"))         #Output: False
+print(name.startswith("21G21"))      #Output: True
+name="ABCDabcd"
+print(name.startswith("ABCD"))       #Output: True
+print(name.startswith("abcd"))       #Output: False
+
+#endswith()
+name="MOUNIKA"
+print(name.endswith("MOUNI"))     #Output: False
+print(name.endswith("NIKA"))      #Output: True
+
+#isalpha()
+name="HELLO"
+print(name.isalpha())    #Output: True
+name="1234"
+print(name.isalpha())    #Output: False
+
+#isalnum()
+name="HELLO"
+print(name.isalnum())     #Output: True
+name="1234"
+print(name.isalnum())     #Output: True
+name="@1234"
+print(name.isalpha())     #Output: False
+
+#islower()
+name="MOULI"
+print(name.islower())     #Output: False
+name="mouli"
+print(name.islower())     #Output: True
+
+#isupper()
+name="MOULI"
+print(name.isupper())     #Output: True
+name="mouli"
+print(name.isupper())     #Output: False
+
+#isspacee()
+name="     "
+print(name.isspace())     #Output: True
+name=""
+print(name.isspace())     #Output: False
+
+#istitle()
+name="print hello"
+print(name.istitle())    #Output: False
+name="Print Hello"
+print(name.istitle())    #Output: True
+
+#isidentifier()
+name="Python_1"
+print(name.isidentifier())   #Output: True
+name="@Python"
+print(name.isidentifier())   #Output: False
+
+
+#7.Replace & Modify Methods
+#replace()
+name="mouni manovar"
+print(name.replace("v","h"))   #Output: mouni manohar
+name="sudha mouni"
+print(name.replace("n","l"))   #Output: sudha mouli
+
+#translate()
+name="abcd"
+print(name.translate(str.maketrans("a","x")))    #Output: xbcd
+
+#maketrans()
+name="python"
+print(name.maketrans("th","#$"))       #Output: {116:,35:,104:,36:}
+
+#8.splitting & Joining Methods
+#split()
+name="x,y,z"
+print(name.split(","))            #Output: ['x','y','z']
