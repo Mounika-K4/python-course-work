ae58f4d20e25587c04a01c02e1f047e612a968bd
Mounika-K4 <kaniyampatimounika2003@gmail.com>
2025-07-12 21:50:32 +0530

    Operators

--- classwork/4.PythonOperators.py
+++ classwork/4.PythonOperators.py
@@ -1,4 +1,4 @@
-#Arthmetic operators
+#1. Arthmetic operators
 a=10
 b=20
 print("Addition(+):",a+b)            #Addition(+): 30
@@ -9,7 +9,7 @@ print("floor division(//):",a//b)    #floor division(//):0
 print("Modulus(%):",a%b)             #Modulud(%):10
 print("Exponentional(**):",a**b)     #Exponentional(**):1000000000000
 
-#Comparision Operators
+#2. Comparision Operators
 a=5                                          
 b=10
 print("Equal to(==):",a==b)                  #statement:False
@@ -19,7 +19,7 @@ print("less than(<):",a<b)                  #Statement:True
 print("Greater than or Equal to(>=):",a<=b)  #Statement:True
 print("less than or equal to(<=):",a<=b)     #Statement:True
 
-#Assignment Operator
+#3. Assignment Operator
 a=10
 print("Assign(=):",a)                   #Assign(=):10
 a=a+5
@@ -35,7 +35,7 @@ print("floor division & Assign(//=):",a)   # floor division & Assign(//=): 4.0
 a=a**5
 print("Exponentional & Assign(**=):",a)    #Exponentional & Assign(**=): 1024.0
 
-#Logical Operator
+#4. Logical Operator
 a=6
 b=10
 print("And:",a>b and b>a and a<b)         #And: False
@@ -43,7 +43,7 @@ print("And:",a%3==0 and b%5==0)           #And: True
 print("or:",a%2==0 or b%3==0)             #or: True
 print("NOT:",not b%3==0)                  #or: True
 
-#Membership Operations
+#5. Membership Operations
 #list
 L=["Mounika","sreeja","harika","bhavana","hema"]
 print("mounika" in L)            #Statement:False
@@ -69,7 +69,7 @@ print("21" in d)                     #Statement:False
 print('course' in d)                 #Statement:True
 print('age' in d)                    #Statement:True
 
-#Identitity Operation
+#6. Identitity Operation
 a=[1,2,3,4]     
 b=[1,2,3,4]
 c=[3,4,5,6]
@@ -78,3 +78,15 @@ print(a is b)                    #Statement:True
 print(a is not c)                #Statement:True
 print(b is c)                    #Statement:False
 print(a is c)                    #Statement:False
+
+#7. Bitwise Operation
+a=4                       #Binary : 0100
+b=5                       #Binary : 0101
+print(a & b)              #Output : 4 (Binary : 0100)
+print(a | b)              #Output : 5 (Binary : 0101)
+print(a ^ b)              #Output : 1 (Binary : 0001)
+print(~ a)                #Output : -5 (inverts bits of 5)
+print(a << 2)             #Output : 16 (Binary : 10000)
+print(a >> 2)             #Output : 1 (Binary : 0001)
+print(b << 2)             #Output : 20 (Binary : 10100)
+print(b >> 2)             #Output : 1 (Binary : 0001)
