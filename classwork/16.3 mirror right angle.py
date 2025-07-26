c2239455ab0e8e94cbd612fadeeaf45aac3e8837
Mounika-K4 <kaniyampatimounika2003@gmail.com>
2025-07-26 22:31:15 +0530

    mirror right angle

---
+++ classwork/16.3 mirror right angle.py
@@ -0,0 +1,20 @@
+n = int(input("Ente the size: "))
+for i in range(n):
+    for j in range(n-i-1):
+        print(' ',end =" ")
+    for j in range(i):
+        print('*',end =" ")
+    print()
+
+#output:Enter the size:
+    '''
+                * 
+              * * 
+            * * * 
+          * * * * 
+        * * * * * 
+      * * * * * * 
+    * * * * * * * 
+  * * * * * * * *
+* * * * * * * * *
+'''
