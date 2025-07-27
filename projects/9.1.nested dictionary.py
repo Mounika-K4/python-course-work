d973d4f79b069fadf908075d09ef66d94e34e0d6
Mounika-K4 <kaniyampatimounika2003@gmail.com>
2025-07-27 17:52:21 +0530

    snake

--- projects/9.1.nested dictionary.py
+++
@@ -1,7 +0,0 @@
-#6.Nested Dictionaries
-students = {
-"hema": {"age": 21,"course": "cs"},
-"mahi": {"age": 22,"course": "math"}
-}
-print(students["hema"]["course"])
-print(students["mahi"]["course"])
