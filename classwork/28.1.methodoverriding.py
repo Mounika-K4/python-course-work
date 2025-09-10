1da02ef0cb44b1e35fafed272d8011841369e152
Mounika-K4 <kaniyampatimounika2003@gmail.com>
2025-09-10 16:45:00 +0530

    method-overriding

---
+++ classwork/28.1.methodoverriding.py
@@ -0,0 +1,44 @@
+class NormalUser():
+    def __init__(self,username):
+        self.username=username
+        print(f'\nWelcome to youtube "{self.username}". explore our content..')
+
+    def playvedio(self):
+        print('Ads  Included')
+        print('No Background Play')
+        print('Download with Low Quality')
+        print('No yt music')
+    def profile(self):
+        print('You can upload your profile')
+    def MultipleDevice(self):
+        print('you can login with different devices')
+    def shorts(self):
+        print('You can see the yt shorts')
+    def likecommentshare(self):
+        print('You can like share and comment')
+
+class PremiumUser(NormalUser):
+    def __init__(self,username):
+        super().__init__(username)
+
+    def playvedio(self):
+        print('Ads Free')
+        print('You can play on Background')
+        print('Exclusive content')
+        print('Download with hign Quality')
+        print('YT music is available')
+
+
+mounika=NormalUser('mounika')
+mounika.playvedio()
+mounika.profile()
+mounika.shorts()
+mounika.MultipleDevice()
+mounika.likecommentshare()
+
+nikhil=PremiumUser('nikhil')
+nikhil.playvedio()
+nikhil.profile()
+nikhil.shorts()
+nikhil.MultipleDevice()
+nikhil.likecommentshare()
