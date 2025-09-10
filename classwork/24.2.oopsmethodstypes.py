829441fbc1ac77afe56e9d2709899e55e4bc0d8e
Mounika-K4 <kaniyampatimounika2003@gmail.com>
2025-09-10 16:07:18 +0530

    opsmethodstypes

---
+++ classwork/24.2.oopsmethodstypes.py
@@ -0,0 +1,26 @@
+'''
+Shopping cart 
+'''
+class Shopping:
+    discount=10
+    cat=['men','women','footware','electronics']
+    def placeorder(self,product):
+        self.product=product
+        print("order is placed")
+        @classmethod
+        def updatediscount(cls,upd_dis):
+            cls.discount=upd_dis
+    
+            print("discount is updated")
+
+        @staticmethod
+        def formatdiscount(discount):
+            print(f"{discount}%discount is availble.shop now!!!")
+            
+        
+mounika=Shopping()
+
+mounika.placeorder("phone")
+
+
+mounika.updatediscount(15)
