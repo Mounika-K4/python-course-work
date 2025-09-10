f808d0f2f10e5348c2fec0bdb2e80552c552b857
Mounika-K4 <kaniyampatimounika2003@gmail.com>
2025-09-10 15:33:19 +0530

    mathmodule

---
+++ classwork/22.3mathmodule.py
@@ -0,0 +1,18 @@
+import math
+print(math.sqrt(25))#5.0
+print(math.factorial(5))#120
+print(math.pi , math.e ) #3.141592653589793  #2.718281828459045
+print(math.pow(2,3))#8.0
+print(math.ceil(12.8)) #13
+print(math.ceil(12.3)) #13
+print(math.floor(12.3)) #12
+print(math.floor(12.8)) #12
+print(math.fabs(-2),abs(-2)) #2.0 2
+print(math.gcd(12, 24)) #12
+print(math.gcd(6, 24)) #6
+print(math.log(2))#0.6931471805599453
+print(math.sin(45))#0.8509035245341184
+print(math.cos(45))#0.5253219888177297
+print(math.tan(45))#1.6197751905438615
+print(math.degrees(45)) #2578.3100780887044
+print(math.radians(45)) #0.7853981633974483
